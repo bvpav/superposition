45f60fbefe51d3fb00fbd42dfdfd96d03ce54b51
bvpav <bvpav@tutanota.com>
2022-09-23 22:08:06 +0300

    broken sim

--- src/pages/three-body-problem.tsx
+++ src/pages/three-body-problem.tsx
@@ -1,4 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+const useAnimationFrame = (callback: (dt: number) => void) => {
+  const requestRef = useRef<ReturnType<typeof requestAnimationFrame>>();
+  const previousTimeRef = useRef<DOMHighResTimeStamp>();
+
+  const animate = (time: DOMHighResTimeStamp) => {
+    if (previousTimeRef.current !== undefined) {
+      const deltaTime = time - previousTimeRef.current;
+      callback(deltaTime);
+    }
+    previousTimeRef.current = time;
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current!);
+  }, []);
+};
 
 const ThreeBodyPage = () => {
   const [positions, setPositions] = useState<[number, number][]>([
@@ -6,11 +25,34 @@ const ThreeBodyPage = () => {
     [-128, -128],
     [128, -128],
   ]);
+
+  useAnimationFrame(() =>
+    setPositions(([...positions]) => {
+      for (const i in positions) {
+        let [ax, ay] = positions[i]!;
+        for (const j in positions) {
+          if (i === j) continue;
+          const [bx, by] = positions[j]!;
+          let [vx, vy] = [bx - ax, by - ay];
+          const mag = Math.sqrt(vx * vx + vy * vy);
+          vx /= mag;
+          vy /= mag;
+          const force = 7000 / ((bx - ax) ** 2 + (by - ay) ** 2);
+          ax += vx * force;
+          ay += vy * force;
+        }
+        positions[i] = [ax, ay];
+      }
+      return positions;
+    })
+  );
+
   return (
     <div className="flex h-[100vh] bg-black">
-      <div className="h-full w-full bg-black">
-        {positions.map(([x, y]) => (
+      <div className=" h-full w-full overflow-clip bg-black">
+        {positions.map(([x, y], i) => (
           <div
+            key={i}
             className="absolute left-[50%] top-[50%] h-10 w-10 rounded-full bg-red-500"
             style={{ transform: `translate(${x}px, ${-y}px)` }}
           />
